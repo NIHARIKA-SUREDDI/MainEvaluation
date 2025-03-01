653fad7ab0fe07eef5093b4351c01abb03f1aabf
NIHARIKA-SUREDDI <niharika.sureddi@gmail.com>
2025-03-01 15:41:43 +0530

    authentication login route implemented

--- routes/Authentication.routes.js
+++ routes/Authentication.routes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
 const userModel = require("../models/user.model");
+const jwt = require("jsonwebtoken");
 const saltRounds = 10;
 const AuthenticationRouter = express.Router();
 
@@ -18,6 +19,33 @@ AuthenticationRouter.post("/auth/register", (req, res) => {
     });
   } catch (err) {
     res.status(500).json({ msg: "error in user register" });
+  }
+});
+
+AuthenticationRouter.post("/auth/login", async (req, res) => {
+  try {
+    let User = await userModel.findOne({ email: req.body.email });
+    if (User) {
+      let myplaintextpassword = req.body.password;
+      let hash = User.password;
+      bcrypt.compare(myplaintextpassword, hash, function (err, result) {
+        if (err) {
+          res.status(500).json({ msg: "something went wrong" });
+        } else {
+          if (result) {
+            var token = jwt.sign(
+              { userid: User._id, role: User.role },
+              process.env.JWT_SECRET
+            );
+            res.status(201).json({ msg: "login success", token });
+          } else {
+            res.status(500).json({ msg: "wrong password please try again" });
+          }
+        }
+      });
+    }
+  } catch (err) {
+    res.status(500).json({ msg: "error in signup" });
   }
 });
 
